0a6c8d4daef3c1e6929d61de7748dc0c392e5365
sohammishra864-wq <sohammishra864@gmail.com>
2026-06-05 16:17:48 +0530

    .

--- Backend/graph/orchestration.py
+++ Backend/graph/orchestration.py
@@ -18,7 +18,7 @@ class Workflow:
         tavily_key = os.getenv("TAVILY_API_KEY", "")
         if not tavily_key:
             logger.warning("Tavily API key is missing from environment variables!")
-            # scout
+            # scout librarian error fixed now api one and db
         self.scout = ScoutAgent(tavily_api_key=tavily_key)
         self.librarian = LibrarianAgent(db_conn=db_conn, chroma_collection=chroma_collection)
         self.graph = self._build_graph()
